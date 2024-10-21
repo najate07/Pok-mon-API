5bb6c0b16fd91a71066b3e37507a6f19a1f3e4ec
kmartin <kmartin@ipsosenso.com>
2024-10-21 14:59:04 +0200

    init db

--- src/main/java/fr/efrei/pokemon/controller/DefaultController.java
+++ src/main/java/fr/efrei/pokemon/controller/DefaultController.java
@@ -1,6 +1,7 @@
 package fr.efrei.pokemon.controller;
 
 
+import fr.efrei.pokemon.models.Pokemon;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class DefaultController {
 
 	@GetMapping
-	public String greetings() {
-		return "Hello World!";
+	public Pokemon greetings() {
+		Pokemon pokemon = new Pokemon();
+		pokemon.setLevel(30);
+		pokemon.setName("Ronflex");
+		pokemon.setType("Normal");
+		return pokemon;
 	}
 }
---
+++ src/main/java/fr/efrei/pokemon/models/Pokemon.java
@@ -0,0 +1,34 @@
+package fr.efrei.pokemon.models;
+
+public class Pokemon {
+
+	private String name;
+
+	private int level;
+
+	private String type;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+}
