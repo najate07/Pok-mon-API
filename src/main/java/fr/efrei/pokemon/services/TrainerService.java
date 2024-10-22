4fa14a7a2e38c37f5f99b09a596a7597e884092a
kmartin <kmartin@ipsosenso.com>
2024-10-22 10:38:34 +0200

    service & repo trainer

---
+++ src/main/java/fr/efrei/pokemon/services/TrainerService.java
@@ -0,0 +1,27 @@
+package fr.efrei.pokemon.services;
+
+import fr.efrei.pokemon.models.Trainer;
+import fr.efrei.pokemon.repositories.TrainerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TrainerService {
+
+	private final TrainerRepository repository;
+
+	@Autowired
+	public TrainerService(TrainerRepository repository) {
+		this.repository = repository;
+	}
+
+	public List<Trainer> findAll() {
+		return repository.findAll();
+	}
+
+	public Trainer findById(String id) {
+		return repository.findById(id).orElse(null);
+	}
+}
